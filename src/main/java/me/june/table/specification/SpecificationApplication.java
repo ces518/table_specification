7b40a46a252b21cb913b66e723c626d996f70048
ces518 <pupupee9@gmail.com>
2019-08-22 10:38:30 +0900

    logic 수정

--- src/main/java/me/june/table/specification/SpecificationApplication.java
+++ src/main/java/me/june/table/specification/SpecificationApplication.java
@@ -20,10 +20,7 @@ import java.io.OutputStream;
 import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @SpringBootApplication
 public class SpecificationApplication {
@@ -55,21 +52,54 @@ public class SpecificationApplication {
     private List createTables(DatabaseMetaData metaData, ResultSet resultSet) throws SQLException {
         List tables = new ArrayList();
         while (resultSet.next()) {
-            String table_name = resultSet.getString("TABLE_NAME");
+            String tableName = resultSet.getString("TABLE_NAME");
             String remarks = resultSet.getString("REMARKS");
-            ResultSet columns = metaData.getColumns(null, null, table_name, null);
             TableMetaData sqlTableMetaData = new TableMetaData();
             sqlTableMetaData.setRemarks(remarks);
-            sqlTableMetaData.setTableName(table_name);
-            sqlTableMetaData.setColumns(createColumns(columns));
+            sqlTableMetaData.setTableName(tableName);
+            sqlTableMetaData.setColumns(createColumns(metaData, tableName));
 
             tables.add(sqlTableMetaData);
         }
         return tables;
     }
 
-    private List createColumns(ResultSet columns) throws SQLException {
+    private List createPrimaryKeys (DatabaseMetaData metaData, String tableName) throws SQLException {
+        List primaryKeys = new ArrayList();
+        ResultSet primaryKeyResults = metaData.getPrimaryKeys(null, null, tableName);
+
+        while (primaryKeyResults.next()) {
+            primaryKeys.add(primaryKeyResults.getString("COLUMN_NAME"));
+        }
+        return primaryKeys;
+    }
+
+    private List createForeignKeys (DatabaseMetaData metaData, String tableName) throws SQLException {
+        List importedKeys = new ArrayList();
+        ResultSet importedKeyResult = metaData.getImportedKeys(null, null, tableName);
+
+        while (importedKeyResult.next()) {
+            importedKeys.add(importedKeyResult.getString("FKCOLUMN_NAME"));
+        }
+        return importedKeys;
+    }
+
+    private List createIndexes (DatabaseMetaData metaData, String tableName) throws SQLException {
+        List indexes = new ArrayList();
+        ResultSet indexResult = metaData.getIndexInfo(null, null, tableName, false, false);
+        while (indexResult.next()) {
+            indexes.add(indexResult.getString("INDEX_NAME"));
+        }
+        return indexes;
+    }
+
+    private List createColumns(DatabaseMetaData metaData, String tableName) throws SQLException {
         List tableColumns = new ArrayList();
+
+        List<String> primaryKeys = createPrimaryKeys(metaData, tableName);
+        List<String> foreignKeys = createForeignKeys(metaData, tableName);
+
+        ResultSet columns = metaData.getColumns(null, null, tableName, null);
         while (columns.next()) {
             ColumnMetaData sqlColumnMetaData = new ColumnMetaData();
             sqlColumnMetaData.setColumnName(columns.getString("COLUMN_NAME").toUpperCase());
@@ -78,9 +108,15 @@ public class SpecificationApplication {
             sqlColumnMetaData.setIsNullable(columns.getString("IS_NULLABLE"));
             sqlColumnMetaData.setColumnSize(columns.getString("COLUMN_SIZE"));
             sqlColumnMetaData.setRemarks(columns.getString("REMARKS"));
+            String isPk = primaryKeys.stream().filter(s -> s.equalsIgnoreCase(sqlColumnMetaData.getColumnName())).findFirst().map(s -> "Y").orElse("");
+            String isFk = foreignKeys.stream().filter(s -> s.equalsIgnoreCase(sqlColumnMetaData.getColumnName())).findFirst().map(s -> "Y").orElse("");
+
+            sqlColumnMetaData.setIsPk(isPk);
+            sqlColumnMetaData.setIsFk(isFk);
             System.out.println(sqlColumnMetaData.toString());
             tableColumns.add(sqlColumnMetaData);
         }
+
         return tableColumns;
     }
 
